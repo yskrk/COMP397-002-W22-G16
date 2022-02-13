dcedf671323d1cf3613834b3e54f0dc0a6b13e2c
JoshuaEagles <joshuaeagles1@gmail.com>
2022-02-13 12:58:07 -0500

    Music + Volume Controls Functionality (doesn't save yet)

--- Assets/[Scripts]/UIControls/OptionsUIControls.cs
+++ Assets/[Scripts]/UIControls/OptionsUIControls.cs
@@ -14,11 +14,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class OptionsUIControls : MonoBehaviour
 {
+	public AudioMixer musicMixer;
+	public AudioMixer soundEffectMixer;
+	public Slider musicVolumeSlider;
+	public Slider soundEffectVolumeSlider;
+
+	void Start()
+	{
+		musicMixer.GetFloat("Volume", out float musicVolume);
+		musicVolumeSlider.value = Mathf.Pow(10, musicVolume / 20);
+
+		soundEffectMixer.GetFloat("Volume", out float soundEffectVolume);
+		soundEffectVolumeSlider.value = Mathf.Pow(10, soundEffectVolume / 20);
+	}
+
 	public void OnBackButton_Pressed()
 	{
 		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void OnMusicVolumeSlider_Changed()
+	{
+		musicMixer.SetFloat("Volume", Mathf.Log10(musicVolumeSlider.value) * 20);
+	}
+
+	public void OnSoundEffectVolumeSlider_Changed()
+	{
+		soundEffectMixer.SetFloat("Volume", Mathf.Log10(soundEffectVolumeSlider.value) * 20);
 	}
 }
